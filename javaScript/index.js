2eb0d7ba55745d47049083e222e15c05a729cd8e
Yash Mittal <82879010+Yashmittal100201@users.noreply.github.com>
2022-03-08 03:06:08 +0530

    Create index.js

---
+++ javaScript/index.js
@@ -0,0 +1,9 @@
+function showStuff() {
+    var x = document.getElementById('about-more');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+     else {
+        x.style.display = "none";
+    }
+}
